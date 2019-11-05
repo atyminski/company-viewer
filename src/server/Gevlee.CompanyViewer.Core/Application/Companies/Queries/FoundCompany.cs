37ebea65a1710d489d34359f95cc91810efed2ed
Arkadiusz Tymiński <tyminski.arek@hotmail.com>
2019-11-05 07:49:27 +0100

    Result fill

--- src/server/Gevlee.CompanyViewer.Core/Application/Companies/Queries/FoundCompany.cs
+++ src/server/Gevlee.CompanyViewer.Core/Application/Companies/Queries/FoundCompany.cs
@@ -1,13 +1,19 @@
-using System;
-
 namespace Gevlee.CompanyViewer.Core.Application.Companies.Queries
 {
     public class FoundCompany
     {
         public int Id { get; set; }
 
+        public string Name { get; set; }
+
         public string Street { get; set; }
 
-        //TODO: Rest of properties
+        public string HouseNumber { get; set; }
+
+        public string AppartmentNumber { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string City { get; set; }
     }
 }
