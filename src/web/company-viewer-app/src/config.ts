b36057be29817013ba65072783468c7769297180
Arkadiusz Tymiński <tyminski.arek@hotmail.com>
2019-11-07 15:56:31 +0100

    web - docker

--- src/web/company-viewer-app/src/config.ts
+++ src/web/company-viewer-app/src/config.ts
@@ -1,5 +1,8 @@
 const defaultSettings = {
-    API_URL: 'http://localhost:5000'
+    API_URL: 'http://localhost'
   }
-const settings = {...defaultSettings, ...process.env}
+const settings = {
+  ...defaultSettings,
+  API_URL: process.env.REACT_APP_API_URL
+}
 export default settings
